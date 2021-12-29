adfa594c50929438b78623d2a092e687a72021d9
Nishanth Reddy <Nishanthreddy179@gmail.com>
2021-12-29 14:31:12 +0530

    Modified edge case scenarios

--- src/Main.java
+++ src/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Please enter the number of items in your cart: ");
         int addToCart = scanner.nextInt();
-        if (addToCart < 0) {                                                            // Checks for negative values
+        if (addToCart <= 0) {                                                            // Checks for negative values
             System.out.println("please enter a positive value");
             addToCart = scanner.nextInt();
         }
@@ -17,10 +17,18 @@ public class Main {
 
 
         int temp = scanner.nextInt();
+        if(temp <=0 || temp >3) {
+            System.out.println("please enter correct value.");
+            temp = scanner.nextInt();
+        }
         switch (temp) {
             case 1:
                 System.out.println("please select the number of items you want to add");
                 int addingItems = scanner.nextInt();
+                if(addingItems == 0){
+                    System.out.println("please select a natural number to add.");
+                    addingItems = scanner.nextInt();
+                }
                 addToCart += addingItems;
                 System.out.println("Addition successful!! you have " + addToCart + " items remaining in your cart");
                 break;
@@ -30,15 +38,22 @@ public class Main {
                 int deletingItems = scanner.nextInt();
                 if (addToCart <= 0) {                                                  //Checks whether deletingItems is < 0
                     System.out.println("Items in cart are " + addToCart + ", cannot initiate the request.");
-                } else if (addToCart < deletingItems) {                                //Checks whether deletingItems is less than addToCart value
+                } else if(deletingItems <= 0){
+                    System.out.println("please select a natural number of items");
+                    deletingItems = scanner.nextInt();
+                }else if (addToCart < deletingItems) {                                //Checks whether deletingItems is less than addToCart value
                     System.out.println("Items in cart are less than " + deletingItems + " cannot initiate the request. Please select the appropriate amount");
-                } else {
+                } else{
                     addToCart -= deletingItems;
                     System.out.println("Deletion successful!! you have " + addToCart + " items remaining in your cart");
                 }
                 break;
 
             case 3:
+                if(addToCart == 0) {
+                    System.out.println("Unable to checkout, please add items to the cart");
+                    break;
+                }
                 System.out.println("Thank you for shopping with us. You have checked out with " + addToCart + " items in your cart. Happy Shopping!!!!");
                 break;
         }
